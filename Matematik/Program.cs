dd1a9cc7cf54f8e2f93d5a1219de78f3447c6e53
Kadir Dağlı <mehmetkadirdagli27@gmail.com>
2023-12-28 19:33:16 +0300

    Proje dosyası ekle.

---
+++ Matematik/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           matematiksel M = new matematiksel();
+            YenidenIslemYap:
+            M.menuHazirla();
+            int kullaniciSecim = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("lutfen bir sayi degeri giriniz");
+            decimal kullaniciSayi1  = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("lutfen 2. sayi degerinin giriniz");
+            decimal kullaniciSayi2 = decimal.Parse(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+            switch(kullaniciSecim)
+            {
+                case 1:
+                   sonuc = M.toplamaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "+");
+                    break;
+                case 2:
+                    sonuc = M.cikarmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "-");
+                    break;
+                case 3:
+                    sonuc = M.carpmaIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "*");
+                    break;
+                case 4:
+                    sonuc = M.bolmeIslemi(kullaniciSayi1, kullaniciSayi2);
+                    M.sonucEkranaYaz(kullaniciSayi1, kullaniciSayi2, sonuc, "/");
+                    break;
+                default:
+                    Console.WriteLine("belirtmiş oldugunuz deger liste içerisinde bulunamadı");
+                    Console.WriteLine("lutfen yeni bir deger giriniz");
+                    System.Threading.Thread.Sleep(2000);
+                    goto YenidenIslemYap;
+            }
+            Console.WriteLine("yeni islem yapmak istiyor musunuz? E/H");
+            string EH = Console.ReadLine();
+            if(EH == "E")
+            {
+                goto YenidenIslemYap;
+            }
+        }
+    }
+}
---
+++ Matematik/matematiksel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    public class matematiksel
+    {
+        public decimal toplamaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+        public decimal cikarmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+        public decimal carpmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+        public void menuHazirla()
+        {
+            Console.WriteLine("***** Menu Hazırla *****");
+            Console.WriteLine("1- toplama");
+            Console.WriteLine("2- cıkarma");
+            Console.WriteLine("3- carpma");
+            Console.WriteLine("4- bolme");
+            Console.Write("Lutfen deger seciniz: ");
+        }
+        public void sonucEkranaYaz(decimal kullaniciSayi1, decimal kullaniciSayi2, decimal sonuc, string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", kullaniciSayi1,operators,kullaniciSayi2, sonuc);
+        }
+    }
+}
